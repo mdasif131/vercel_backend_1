dd8306434cacefd3f016322b2322511657af0042
MD ASIF CHOWDHURY <asifmdasifchy@gmail.com>
2026-01-07 03:50:56 +0600

    Update createToken.js

--- utils/createToken.js
+++ utils/createToken.js
@@ -8,8 +8,9 @@ const generateToken = (res, userId) => {
   // Set token in HTTP-only cookie
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    // secure: process.env.NODE_ENV !== "development",
+    secure: isProd,
+    sameSite:  isProd ? "none" : "lax",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 return token;
